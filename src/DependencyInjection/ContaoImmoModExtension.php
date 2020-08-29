f4d6d47d09a629db96620c39d06d55469de14611
Dshosev <josef.f.unger@googlemail.com>
2020-08-29 16:27:43 +0200

    Fixes typo in namespace of dependency injection

--- src/DependencyInjection/ContaoImmoModExtension.php
+++ src/DependencyInjection/ContaoImmoModExtension.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Nextbrand\ContaoImmoModBundle\DependencyInjection;
+namespace Nextbrand\ContaoImmoModExtension\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
